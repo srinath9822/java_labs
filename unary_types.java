2b083258a14d3f434dc1812210f82ce301508ef8
srinath9822 <116694532+srinath9822@users.noreply.github.com>
2022-10-26 07:22:40 +0530

    Create unary_types.java

---
+++ unary_types.java
@@ -0,0 +1,13 @@
+public class Unary_types {
+		public static void main(String[] args) {
+			int a=10;
+			 
+			    //Increment operator
+				System.out.println("Post Increment: " +a++); 
+				System.out.println("Pre Increment: "+(++a)); 
+				
+				// Decrement operator
+				System.out.println("Post Decrement: " +a--);
+				System.out.println("Pre Decrement: "+(--a)); 
+
+	}
